5abd9c5933d93e581e69a10c3cce3ede1b555c3e
cezhou3 <cezhou@hust.edu.cn>
2025-06-30 05:05:13 +0000

    update install bash

--- install.sh
+++ install.sh
@@ -10,5 +10,7 @@ conda activate pubmedclip
 # install requirements
 cd ./PubMedCLIP
 pip install -r requirements.txt
-pip uninstall torch torchvision
-pip install torch==1.10.0+cu111 torchvision==0.11.0+cu111 torchaudio==0.10.0 -f https://download.pytorch.org/whl/torch_stable.html
+pip uninstall -y setuptools
+pip install setuptools==59.5.0
+pip install torch==1.13.1+cu117 torchvision==0.14.1+cu117 torchaudio==0.13.1 --extra-index-url https://download.pytorch.org/whl/cu117
+pip install git+https://github.com/openai/CLIP.git
